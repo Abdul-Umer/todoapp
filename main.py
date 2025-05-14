3413a64349752e13f765b53b9e12750a8d8243d0
Abdul-Umer <abdulumer1205@gmail.com>
2025-05-14 22:05:14 +0530

    second commit

--- main.py
+++ main.py
@@ -104,9 +104,9 @@ def completedTasks():
 
 
 
-@app.route('/add/<int:a>,<int:b>')
-def add(a,b):
-    return str(a)+str(b)
+@app.route('/add/<int:a>,<int:b>,<int:c>')
+def add(a,b,c):
+    return str(a)+str(b)+str(c)
 
 if __name__ == "__main__":
     app.run(debug=True)
